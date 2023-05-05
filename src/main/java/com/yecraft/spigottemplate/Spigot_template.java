3a97e07f519abc2e1c7e83646ffe85d16bc5e7e4
IStarred <balamutmaks@gmail.com>
2023-05-05 17:49:30 +0300

    Initial commit

---
+++ src/main/java/com/yecraft/spigottemplate/Spigot_template.java
@@ -0,0 +1,17 @@
+package com.yecraft.spigottemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spigot_template extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
